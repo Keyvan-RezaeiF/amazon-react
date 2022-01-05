6539ae24438001312ea089243aba34bd990d35bb
Keyvan-RezaeiF <k1rzfi80@gmail.com>
2022-01-05 21:40:04 +0330

    compelete Home

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Home.css'
+import Product from './Product'
 
 function Home() {
     return (
@@ -11,18 +12,67 @@ function Home() {
                 />
 
                 <div className="home_row">
-                    {/* Product */}
-                    {/* Product */}
+                    <Product
+                        id="12321341"
+                        title='The lean startup'
+                        price={29.99}
+                        image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
+                        rating={5}
+                    />
+                    <Product
+                        id="49538094"
+                        title='Wireless Rechargeable Battery
+                        Powered Security Camera Outdoor
+                        , 1080P 2.4G WiFi IP Camera with
+                        Night Vision, Motion Detection'
+                        price={239.0}
+                        image="https://images-na.ssl-images-amazon.com/images/I/61N67x8EAqL._AC_UL450_SR450,320_.jpg"
+                        rating={4}
+                    />
                 </div>
 
                 <div className="home_row">
-                    {/* Product */}
-                    {/* Product */}
-                    {/* Product */}
+                <Product
+                        id="86426345"
+                        title='SEAAES Ice Scraper with Snow Brush
+                            for Car Windshield, Extendable Snow
+                            Brush with ... '
+                        price={62.93}
+                        image="https://images-na.ssl-images-amazon.com/images/I/51s0PxpWuSL._AC_UL450_SR450,320_.jpg"
+                        rating={2}
+                    />
+                    <Product
+                        id="83453347"
+                        title='LORYERGO Adjustable Laptop Stand,
+                            Portable Laptop Riser for 17.3inch
+                            Laptops, Adjustment Laptop Stand ...'
+                        price={21.99}
+                        image="https://images-na.ssl-images-amazon.com/images/I/61KiPy7h5YL._AC_UL450_SR450,320_.jpg"
+                        rating={5}
+                    />
+                    <Product
+                        id="15739462"
+                        title='Reolink E1 Outdoor WiFi Security
+                            Camera, 5MP HD PTZ Camera for Home
+                            Security, Auto Tracking ... '
+                        price={175.5}
+                        image="https://images-na.ssl-images-amazon.com/images/I/51nVsRPSUiL._AC_UL604_SR604,400_.jpg"
+                        rating={3}
+                    />
                 </div>
 
                 <div className="home_row">
-                    {/* Product */}
+                <Product
+                        id="24967182"
+                        title='Gskyer Telescope, 70mm Aperture
+                            400mm AZ Mount Astronomical Refracting
+                            Telescope for Kids Beginners - Travel
+                            Telescope with Carry Bag, Phone Adapter
+                            and Wireless Remote'
+                        price={119.9}
+                        image="https://images-na.ssl-images-amazon.com/images/I/7133ChhszhL._AC_UL450_SR450,320_.jpg"
+                        rating={4}
+                    />
                 </div>
             </div>
         </div>
