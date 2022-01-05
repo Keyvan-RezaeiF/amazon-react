2b4430e3b25b2dec0ded20613e27df169c26b19e
Keyvan-RezaeiF <k1rzfi80@gmail.com>
2022-01-05 16:25:45 +0330

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD7cRA2hSLLvFUt4oJqs_WBnD1oWR2gi-Y",
+    authDomain: "react-5c0f2.firebaseapp.com",
+    projectId: "react-5c0f2",
+    storageBucket: "react-5c0f2.appspot.com",
+    messagingSenderId: "59189288251",
+    appId: "1:59189288251:web:16407b6387604ce97c2b1e"
+};
