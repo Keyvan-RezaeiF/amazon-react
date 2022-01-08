db255aa570894f0d67562cefdbe29ac6ce1f5e57
Keyvan-RezaeiF <k1rzfi80@gmail.com>
2022-01-08 12:59:03 +0330

    ADD: checkout component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import './App.css';
 import Header from './Header';
 import Home from './Home';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Checkout from './Checkout';
 
 function App() {
   return (
     <div className="app">
-
-      <Header />
-
-      <Home />
-
+      <Router>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/checkout' element={<Checkout />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
